c212c8a954a31b54bf6e697366be5f143b588350
Ronny Ancorini <ronnyancorini@gmail.com>
2023-05-18 21:37:50 +0200

    style: show more relevant information about the pomodoro

--- dashnny-raycast/dashnny/src/remaining.tsx
+++ dashnny-raycast/dashnny/src/remaining.tsx
@@ -1,14 +1,22 @@
 import { Detail, getPreferenceValues } from "@raycast/api";
+import { DateTime } from "luxon";
 import fetch from "node-fetch";
 import { useEffect, useState } from "react";
 
 type Pomodoro = {
+	label: string;
+	id: string;
   endTime: string;
 };
 
+type ActivePomodoro = {
+	remainingTime: string;
+	label: string;
+	id: string;
+} | null | "unitialized";
 
 export default function Command() {
-  const [activePomodoro, setActivePomodoro] = useState<Pomodoro>({endTime: ""});
+  const [activePomodoro, setActivePomodoro] = useState<ActivePomodoro>("unitialized");
 
 	useEffect(() => {
 		fetch(`${getPreferenceValues().dashnnyApiUrl}/Pomodoros?isActive=true`, {
@@ -22,12 +30,33 @@ export default function Command() {
 		.then((pomodorosParam) => {
 			const pomodoros = pomodorosParam as Pomodoro[]
 			if (pomodoros.length > 0) {
-				setActivePomodoro(pomodoros[0]);
+				const pomodoro = pomodoros[0];
+				const diff = DateTime.fromISO(pomodoro.endTime).diff(DateTime.now(), ['hours', 'minutes', 'seconds']);
+				const formattedDiff = `${diff.hours.toString().padStart(2, '0')}:${diff.minutes.toString().padStart(2, '0')}:${Math.floor(diff.seconds).toString().padStart(2, '0')}`;
+				setActivePomodoro({
+					id: pomodoro.id,
+					label: pomodoro.label,
+					remainingTime: formattedDiff
+				});
+				return;
 			}
+			setActivePomodoro(null);
 		})
 	}, []);
 
   return (
-		<Detail markdown={ `${activePomodoro?.endTime ?? "No pomodoro active"}` }></Detail>
+		<>
+		{ activePomodoro == null ? 
+			(<Detail markdown="No timer active"></Detail>) :
+			( activePomodoro == "unitialized" ? 
+				<Detail markdown="Loading..."></Detail> :
+				<Detail markdown={`
+## Working on [${activePomodoro.label}]
+Remaining time: ${activePomodoro.remainingTime}
+
+Pomodoro id: ${activePomodoro.id}
+`}></Detail>)
+		}
+		</>
   );
 }
